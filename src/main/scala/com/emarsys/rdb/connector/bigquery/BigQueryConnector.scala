08eeb14d1e7530ddf5a90a40632bd56ed052e0b7
khorvath <kristof.horvath@emarsys.com>
2018-03-07 15:18:58 +0100

    [search] disabled search feature

--- src/main/scala/com/emarsys/rdb/connector/bigquery/BigQueryConnector.scala
+++ src/main/scala/com/emarsys/rdb/connector/bigquery/BigQueryConnector.scala
@@ -1,12 +1,16 @@
 package com.emarsys.rdb.connector.bigquery
 
+import akka.NotUsed
 import akka.actor.{ActorRef, ActorSystem, PoisonPill}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
 import akka.util.Timeout
 import com.emarsys.rdb.connector.bigquery.BigQueryConnector.BigQueryConnectionConfig
 import com.emarsys.rdb.connector.common.ConnectorResponse
+import com.emarsys.rdb.connector.common.models.DataManipulation.Criteria
 import com.emarsys.rdb.connector.common.models.{CommonConnectionReadableData, _}
+import com.emarsys.rdb.connector.common.notImplementedOperation
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
@@ -26,6 +30,8 @@ class BigQueryConnector(protected val actorSystem: ActorSystem, val config: BigQ
 
   val googleTokenActor: ActorRef = actorSystem.actorOf(GoogleTokenActor.props(config.clientEmail, config.privateKey, Http()))
   val bigQueryClient: BigQueryClient = new BigQueryClient(googleTokenActor, config.projectId, config.dataset)
+
+  override protected def rawSearch(tableName: String, criteria: Criteria, limit: Option[Int]): ConnectorResponse[Source[Seq[String], NotUsed]] = notImplementedOperation
 
   override def close(): Future[Unit] = {
     googleTokenActor ! PoisonPill

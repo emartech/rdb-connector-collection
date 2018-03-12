33d0a881ed80e001aa83499c3a27d9e5d66ce2a2
Robert Doczi <rdoczi@emarsys.com>
2018-03-12 10:34:36 +0100

    [bigquery-client] delaying too much time

--- src/test/scala/com/emarsys/rdb/connector/bigquery/stream/util/DelaySpec.scala
+++ src/test/scala/com/emarsys/rdb/connector/bigquery/stream/util/DelaySpec.scala
@@ -25,7 +25,7 @@ class DelaySpec
       val elems = 0 to 12
       val probe = Source(elems)
         .map(_ => System.nanoTime())
-        .via(Delay(_ => false, TimeUnit.MILLISECONDS))
+        .via(Delay(_ => false, 1000, TimeUnit.MILLISECONDS))
         .map(start => System.nanoTime() - start)
         .runWith(TestSink.probe)
 
@@ -43,23 +43,41 @@ class DelaySpec
 
     "delay elements using fibonacci series" in {
 
-      val elems = 0 to 12
+      val elems  = 0 to 12
       val delays = List(1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144).map(_.millis)
       val probe = Source(elems)
         .map(_ => System.nanoTime())
-        .via(Delay(_ => true, TimeUnit.MILLISECONDS))
+        .via(Delay(_ => true, 1000, TimeUnit.MILLISECONDS))
         .map(start => System.nanoTime() - start)
         .runWith(TestSink.probe)
 
-      (elems zip delays).foreach{case (_, delay) =>
-        val next = probe
-          .request(1)
-          .expectNext()
+      (elems zip delays).foreach {
+        case (_, delay) =>
+          val next = probe
+            .request(1)
+            .expectNext()
 
-        next should be >= delay.toNanos
+          next should be >= delay.toNanos
       }
 
     }
+
+    "throw IllegalStateException when maxDelay is exceeded" in {
+
+      val elems = 0 to 12
+
+      val probe = Source(elems)
+        .map(_ => System.nanoTime())
+        .via(Delay(_ => true, 10, TimeUnit.MILLISECONDS))
+        .map(start => System.nanoTime() - start)
+        .runWith(TestSink.probe)
+
+      probe.request(6).expectNextN(6)
+
+      val error = probe.request(1).expectError()
+      error shouldBe an[IllegalStateException]
+    }
+
   }
 
 }

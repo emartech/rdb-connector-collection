55f13dbce69e5f8d135822bcd55aa9ceea16778a
Soti Csenge <csenge.soti@emarsys.com>
2018-10-15 16:30:33 +0200

    RDS-233: default error handling with ErrorWithMessage

--- src/main/scala/com/emarsys/rdb/connector/bigquery/BigQueryErrorHandling.scala
+++ src/main/scala/com/emarsys/rdb/connector/bigquery/BigQueryErrorHandling.scala
@@ -2,7 +2,7 @@ package com.emarsys.rdb.connector.bigquery
 
 import akka.NotUsed
 import akka.stream.scaladsl.Source
-import com.emarsys.rdb.connector.common.models.Errors.{ConnectionError, ConnectorError, QueryTimeout}
+import com.emarsys.rdb.connector.common.models.Errors.{ConnectionError, ConnectorError, ErrorWithMessage, QueryTimeout}
 
 import scala.concurrent.TimeoutException
 
@@ -11,7 +11,7 @@ trait BigQueryErrorHandling {
   protected def errorHandler: PartialFunction[Throwable, ConnectorError] = {
     case ce: ConnectorError   => ce
     case to: TimeoutException => QueryTimeout(to.getMessage)
-    case ex: Exception        => ConnectionError(ex)
+    case ex: Exception        => ErrorWithMessage(ex.getMessage)
   }
 
   protected def eitherErrorHandler[T]: PartialFunction[Throwable, Either[ConnectorError, T]] =

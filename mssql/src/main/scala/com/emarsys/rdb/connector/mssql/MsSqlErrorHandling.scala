ec48899f2b3c9354239961e4b60e510aa477d89b
khorvath <kristof.horvath@emarsys.com>
2018-11-27 11:09:57 +0100

    RDS-OPS: add explain permission denied error handling

--- mssql/src/main/scala/com/emarsys/rdb/connector/mssql/MsSqlErrorHandling.scala
+++ mssql/src/main/scala/com/emarsys/rdb/connector/mssql/MsSqlErrorHandling.scala
@@ -10,28 +10,31 @@ import com.microsoft.sqlserver.jdbc.SQLServerException
 
 trait MsSqlErrorHandling {
 
-  val MSSQL_STATE_QUERY_CANCELLED     = "HY008"
-  val MSSQL_STATE_SYNTAX_ERROR        = "S0001"
-  val MSSQL_STATE_PERMISSION_DENIED   = "S0005"
-  val MSSQL_STATE_INVALID_OBJECT_NAME = "S0002"
+  val MSSQL_STATE_QUERY_CANCELLED            = "HY008"
+  val MSSQL_STATE_SYNTAX_ERROR               = "S0001"
+  val MSSQL_STATE_SHOWPLAN_PERMISSION_DENIED = "S0004"
+  val MSSQL_STATE_PERMISSION_DENIED          = "S0005"
+  val MSSQL_STATE_INVALID_OBJECT_NAME        = "S0002"
+
   val MSSQL_BAD_HOST_ERROR = "08S01"
 
-  val MSSQL_EXPLAIN_PERMISSION_DENIED   = "EXPLAIN/SHOW can not be issued; lacking privileges for underlying table"
+  val MSSQL_EXPLAIN_PERMISSION_DENIED = "lacking privileges"
 
   val connectionErrors = Seq(
     MSSQL_BAD_HOST_ERROR
   )
 
   protected def errorHandler(): PartialFunction[Throwable, ConnectorError] = {
-    case ex: RejectedExecutionException                                              => TooManyQueries(ex.getMessage)
-    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_QUERY_CANCELLED     => QueryTimeout(ex.getMessage)
-    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_SYNTAX_ERROR        => SqlSyntaxError(ex.getMessage)
-    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_PERMISSION_DENIED   => AccessDeniedError(ex.getMessage)
-    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_INVALID_OBJECT_NAME => TableNotFound(ex.getMessage)
-    case ex: SQLException if ex.getMessage == MSSQL_EXPLAIN_PERMISSION_DENIED        => AccessDeniedError(ex.getMessage)
-    case ex: SQLException if connectionErrors.contains(ex.getSQLState)               => ConnectionError(ex)
-    case ex: SQLException                                                            => ErrorWithMessage(s"[${ex.getSQLState}] - ${ex.getMessage}")
-    case ex: Exception                                                               => ErrorWithMessage(ex.getMessage)
+    case ex: RejectedExecutionException                                                     => TooManyQueries(ex.getMessage)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_QUERY_CANCELLED            => QueryTimeout(ex.getMessage)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_SYNTAX_ERROR               => SqlSyntaxError(ex.getMessage)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_PERMISSION_DENIED          => AccessDeniedError(ex.getMessage)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_INVALID_OBJECT_NAME        => TableNotFound(ex.getMessage)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_SHOWPLAN_PERMISSION_DENIED => AccessDeniedError(ex.getMessage)
+    case ex: SQLException if ex.getMessage.contains(MSSQL_EXPLAIN_PERMISSION_DENIED)        => AccessDeniedError(ex.getMessage)
+    case ex: SQLException if connectionErrors.contains(ex.getSQLState)                      => ConnectionError(ex)
+    case ex: SQLException                                                                   => ErrorWithMessage(s"[${ex.getSQLState}] - ${ex.getMessage}")
+    case ex: Exception                                                                      => ErrorWithMessage(ex.getMessage)
   }
 
   protected def eitherErrorHandler[T](): PartialFunction[Throwable, Either[ConnectorError, T]] =

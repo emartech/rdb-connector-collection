2523074c75e6bb77225524e0f845cf03468e82e5
Miklos Martin <miklos.martin@gmail.com>
2019-02-20 20:52:21 +0100

    CDP-032 add a reusable error converter

---
+++ common/src/main/scala/com/emarsys/rdb/connector/common/defaults/ErrorConverter.scala
@@ -0,0 +1,22 @@
+package com.emarsys.rdb.connector.common.defaults
+
+import java.sql.{SQLException, SQLSyntaxErrorException}
+import java.util.concurrent.{RejectedExecutionException, TimeoutException}
+import com.emarsys.rdb.connector.common.models.Errors._
+
+object ErrorConverter {
+  val common: PartialFunction[Throwable, ConnectorError] = {
+    case e: ConnectorError             => e
+    case e: RejectedExecutionException => TooManyQueries(e.getMessage)
+    case e: TimeoutException           => CompletionTimeout(e.getMessage)
+    case e                             => ErrorWithMessage(e.toString)
+  }
+
+  val sql: PartialFunction[Throwable, ConnectorError] = {
+    case e: RejectedExecutionException if e.getMessage.contains("active threads = 0") => StuckedPool(e.getMessage)
+    case e: SQLSyntaxErrorException                                                   => SqlSyntaxError(e.getMessage)
+    case e: SQLException                                                              => ErrorWithMessage(s"[${e.getSQLState}] - ${e.getMessage}")
+  }
+
+  val default = sql orElse common
+}
---
+++ common/src/test/scala/com/emarsys/rdb/connector/common/defaults/ErrorConverterSpec.scala
@@ -0,0 +1,42 @@
+package com.emarsys.rdb.connector.common.defaults
+
+import java.sql.{SQLException, SQLSyntaxErrorException}
+import java.util.concurrent.{RejectedExecutionException, TimeoutException}
+
+import com.emarsys.rdb.connector.common.models.Errors._
+import org.scalatest.{Matchers, WordSpecLike}
+
+class ErrorConverterSpec extends WordSpecLike with Matchers {
+  "The default common error converter" should {
+    "not touch ConnectorErrors" in {
+      ErrorConverter.common(TooManyQueries("a")) shouldBe TooManyQueries("a")
+    }
+
+    "convert RejectedExecutionExceptions to TooManyQueries" in {
+      ErrorConverter.common(new RejectedExecutionException("msg")) shouldBe TooManyQueries("msg")
+    }
+
+    "convert TimeoutException to CompletionTimeout" in {
+      ErrorConverter.common(new TimeoutException("msg")) shouldBe CompletionTimeout("msg")
+    }
+
+    "convert all other errors to ErrorWithMessage" in {
+      ErrorConverter.common(new Exception("msg")) shouldBe ErrorWithMessage("java.lang.Exception: msg")
+    }
+  }
+
+  "The default SQL error converter" should {
+    "recognize syntax errors" in {
+      ErrorConverter.sql(new SQLSyntaxErrorException("msg")) shouldBe SqlSyntaxError("msg")
+    }
+
+    "rephrase SQLExceptions" in {
+      ErrorConverter.sql(new SQLException("msg", "state")) shouldBe ErrorWithMessage("[state] - msg")
+    }
+
+    "recognize stucked pools" in {
+      val msg = "Task ... rejected from slick.util.AsyncExecutor...[Running, .... active threads = 0, ...]"
+      ErrorConverter.sql(new RejectedExecutionException(msg)) shouldBe StuckedPool(msg)
+    }
+  }
+}

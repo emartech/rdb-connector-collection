30da073124d86e4edf8bf0226c66ecbff3d942cb
Gabor Fulop <gabor.fulop@emarsys.com>
2019-04-16 16:28:19 +0200

    CDP-064: add custom apply functions to some WhereCondition descendant

--- common/src/main/scala/com/emarsys/rdb/connector/common/models/SimpleSelect.scala
+++ common/src/main/scala/com/emarsys/rdb/connector/common/models/SimpleSelect.scala
@@ -2,11 +2,13 @@ package com.emarsys.rdb.connector.common.models
 
 import com.emarsys.rdb.connector.common.models.SimpleSelect.{Fields, TableName, WhereCondition}
 
-case class SimpleSelect(fields: Fields,
-                        table: TableName,
-                        where: Option[WhereCondition] = None,
-                        limit: Option[Int] = None,
-                        distinct: Option[Boolean] = None)
+case class SimpleSelect(
+    fields: Fields,
+    table: TableName,
+    where: Option[WhereCondition] = None,
+    limit: Option[Int] = None,
+    distinct: Option[Boolean] = None
+)
 
 object SimpleSelect {
 
@@ -24,11 +26,30 @@ object SimpleSelect {
 
   case class SpecificFields(fields: Seq[FieldName]) extends Fields
 
+  object SpecificFields {
+    def apply(field: FieldName, otherFields: FieldName*): SpecificFields = SpecificFields(field +: otherFields)
+    def apply(field: String, otherFields: String*): SpecificFields =
+      SpecificFields((field +: otherFields.toList).map(FieldName))
+  }
+
   case class And(conditions: Seq[WhereCondition]) extends WhereCondition
+
+  object And {
+    def apply(condition: WhereCondition, otherConditions: WhereCondition*): And = And((condition +: otherConditions).toList)
+  }
 
   case class Or(conditions: Seq[WhereCondition]) extends WhereCondition
 
+  object Or {
+    def apply(condition: WhereCondition, otherConditions: WhereCondition*): Or = Or((condition +: otherConditions).toList)
+  }
+
   case class EqualToValue(field: FieldName, value: Value) extends WhereCondition
+
+  object EqualToValue {
+    def apply(field: String, value: String): EqualToValue = EqualToValue(FieldName(field), Value(value))
+    def tupled(fv: (FieldName, Value))                    = EqualToValue.apply(fv._1, fv._2)
+  }
 
   case class NotNull(field: FieldName) extends WhereCondition
 

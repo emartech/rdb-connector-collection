db4ebf81a3737aa1d1c212be5caa65ddc9f1c8de
Robert Laszlo Doczi <doczi.r@gmail.com>
2019-06-06 20:05:06 +0200

    RDS-OPS: encode cause in error message returned from mysql connector

--- mysql/src/main/scala/com/emarsys/rdb/connector/mysql/MySqlErrorHandling.scala
+++ mysql/src/main/scala/com/emarsys/rdb/connector/mysql/MySqlErrorHandling.scala
@@ -29,15 +29,34 @@ trait MySqlErrorHandling {
       } else {
         ErrorWithMessage(ex.getMessage)
       }
-    case ex: SQLSyntaxErrorException if ex.getMessage.contains("Access denied")      => AccessDeniedError(ex.getMessage)
-    case ex: MySQLTimeoutException if ex.getMessage.contains("cancelled")            => QueryTimeout(ex.getMessage)
-    case ex: MySQLTimeoutException                                                   => ConnectionTimeout(ex.getMessage)
-    case ex: SQLTransientConnectionException if ex.getMessage.contains("timed out")  => ConnectionTimeout(ex.getMessage)
-    case ex: SQLException if ex.getMessage.contains(MYSQL_EXPLAIN_PERMISSION_DENIED) => AccessDeniedError(ex.getMessage)
+    case ex: SQLSyntaxErrorException if ex.getMessage.contains("Access denied") =>
+      AccessDeniedError(getErrorMessage(ex))
+    case ex: MySQLTimeoutException if ex.getMessage.contains("cancelled") => QueryTimeout(getErrorMessage(ex))
+    case ex: MySQLTimeoutException                                        => ConnectionTimeout(getErrorMessage(ex))
+    case ex: SQLTransientConnectionException if ex.getMessage.contains("timed out") =>
+      ConnectionTimeout(getErrorMessage(ex))
+    case ex: SQLException if ex.getMessage.contains(MYSQL_EXPLAIN_PERMISSION_DENIED) =>
+      AccessDeniedError(getErrorMessage(ex))
     case ex: SQLException if ex.getMessage.contains(MYSQL_STATEMENT_CLOSED) =>
       InvalidDbOperation(s"Transient DB error: ${ex.toString}")
     case ex: SQLException if ex.getMessage.startsWith(MYSQL_ILLEGAL_MIX_OF_COLLATIONS) => SqlSyntaxError(ex.toString)
     case ex: SQLException if ex.getMessage.contains(MYSQL_CONNECTION_HOST_ERROR)       => ConnectionTimeout(ex.toString)
+  }
+
+  private def getErrorMessage(ex: Exception): String = {
+    val message       = ex.getMessage
+    val causeMessages = getCauseMessages(ex).mkString("\n")
+
+    s"$message\n$causeMessages"
+  }
+
+  private def getCauseMessages(ex: Throwable): List[String] = {
+    val cause = ex.getCause
+    if (cause != null) {
+      s"caused by: ${cause.getMessage}" :: getCauseMessages(cause)
+    } else {
+      Nil
+    }
   }
 
   protected def eitherErrorHandler[T](): PartialFunction[Throwable, Either[ConnectorError, T]] =

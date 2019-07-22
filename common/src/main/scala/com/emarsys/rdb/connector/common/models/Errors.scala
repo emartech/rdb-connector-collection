59a682d59c39083e802587280938890dc77c2ae5
Miklos Martin <miklos.martin@gmail.com>
2019-07-22 15:05:19 +0200

    CDP-148 add context to payload

--- common/src/main/scala/com/emarsys/rdb/connector/common/models/Errors.scala
+++ common/src/main/scala/com/emarsys/rdb/connector/common/models/Errors.scala
@@ -3,6 +3,7 @@ package com.emarsys.rdb.connector.common.models
 import enumeratum._
 
 import scala.collection.immutable
+import scala.reflect._
 
 object Errors {
 
@@ -33,6 +34,7 @@ object Errors {
 
   sealed trait ErrorCategory extends EnumEntry
   object ErrorCategory extends Enum[ErrorCategory] {
+    case object Validation extends ErrorCategory
     case object Timeout extends ErrorCategory
 
     override def values: immutable.IndexedSeq[ErrorCategory] = findValues
@@ -43,14 +45,29 @@ object Errors {
   }
   object ErrorName extends Enum[ErrorName] {
     case object QueryTimeout extends ErrorName
+    case object MissingFields extends ErrorName
 
     override def values: immutable.IndexedSeq[ErrorName] = findValues
   }
 
   case class Cause(message: String)
-  case class ErrorPayload(errorCategory: ErrorCategory, error: String, message: String, causes: List[Cause]) extends ConnectorError(message)
+  case class ErrorPayload(errorCategory: ErrorCategory, error: String, message: String, causes: List[Cause], context: Option[Context]) extends ConnectorError(message) {
+    def maybeContext[C <: Context : ClassTag]: Option[C] = context collect { case c:C => c }
+    def ensureContext[C <: Context : ClassTag]: C = maybeContext[C].getOrElse(throw new ContextMismatch[C](context))
+  }
+
   object ErrorPayload {
-    def apply(errorCategory: ErrorCategory, error: ErrorName, message: String, causes: List[Cause]): ErrorPayload =
-      new ErrorPayload(errorCategory, error.entryName, message, causes)
+    def apply(errorCategory: ErrorCategory, error: ErrorName, message: String, causes: List[Cause], context: Option[Context]): ErrorPayload =
+      ErrorPayload(errorCategory, error.entryName, message, causes, context)
+  }
+
+  sealed trait Context
+  case class Fields(fields: List[String]) extends Context
+
+  case class ContextMismatch[C <: Context : ClassTag](context: Option[Context]) extends Throwable {
+    val expected = classTag[C].runtimeClass.getSimpleName
+    val actual = context.map(_.getClass().getSimpleName).getOrElse("None")
+
+    override def getMessage(): String = s"Expected `$expected`, found `$actual`"
   }
 }

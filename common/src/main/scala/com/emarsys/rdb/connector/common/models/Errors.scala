25d51261d9f336963f67f811b356faefa76ec04d
Laszlo Konya <laszlo.konya@emarsys.com>
2019-08-08 15:02:05 +0200

    RDS-OPS: format

--- common/src/main/scala/com/emarsys/rdb/connector/common/models/Errors.scala
+++ common/src/main/scala/com/emarsys/rdb/connector/common/models/Errors.scala
@@ -34,13 +34,13 @@ object Errors {
 
   sealed trait ErrorCategory extends EnumEntry
   object ErrorCategory extends Enum[ErrorCategory] {
-    case object Validation extends ErrorCategory
-    case object Timeout extends ErrorCategory
-    case object RateLimit extends ErrorCategory
+    case object Validation          extends ErrorCategory
+    case object Timeout             extends ErrorCategory
+    case object RateLimit           extends ErrorCategory
     case object FatalQueryExecution extends ErrorCategory
-    case object Transient extends ErrorCategory
-    case object Internal extends ErrorCategory
-    case object Unknown extends ErrorCategory
+    case object Transient           extends ErrorCategory
+    case object Internal            extends ErrorCategory
+    case object Unknown             extends ErrorCategory
 
     override def values: immutable.IndexedSeq[ErrorCategory] = findValues
   }
@@ -52,43 +52,43 @@ object Errors {
     // ======================================
     //              Validation
     // ======================================
-    case object MissingFields extends ErrorName
-    case object DifferentFields extends ErrorName
-    case object EmptyData extends ErrorName
-    case object TooManyRows extends ErrorName
-    case object NonExistingTable extends ErrorName
-    case object NoIndexOnFields extends ErrorName
-    case object EmptyCriteria extends ErrorName
+    case object MissingFields          extends ErrorName
+    case object DifferentFields        extends ErrorName
+    case object EmptyData              extends ErrorName
+    case object TooManyRows            extends ErrorName
+    case object NonExistingTable       extends ErrorName
+    case object NoIndexOnFields        extends ErrorName
+    case object EmptyCriteria          extends ErrorName
     case object InvalidOperationOnView extends ErrorName
-    case object ValidationFailed extends ErrorName
+    case object ValidationFailed       extends ErrorName
     // ======================================
     //              Timeout
     // ======================================
     case object ConnectionTimeout extends ErrorName
     case object CompletionTimeout extends ErrorName
-    case object QueryTimeout extends ErrorName
+    case object QueryTimeout      extends ErrorName
     // ======================================
     //              RateLimit
     // ======================================
     case object TooManyQueries extends ErrorName
-    case object StuckPool extends ErrorName
+    case object StuckPool      extends ErrorName
     // ======================================
     //              FatalQueryExecution
     // ======================================
-    case object TableNotFound extends ErrorName
-    case object SqlSyntaxError extends ErrorName
-    case object AccessDeniedError extends ErrorName
+    case object TableNotFound           extends ErrorName
+    case object SqlSyntaxError          extends ErrorName
+    case object AccessDeniedError       extends ErrorName
     case object NotImplementedOperation extends ErrorName
-    case object SSLError extends ErrorName
+    case object SSLError                extends ErrorName
     // ======================================
     //              Transient
     // ======================================
     case object CommunicationsLinkFailure extends ErrorName
-    case object TransientDbError extends ErrorName
+    case object TransientDbError          extends ErrorName
     // ======================================
     //              Internal
     // ======================================
-    case object ConnectionConfigError extends ErrorName
+    case object ConnectionConfigError            extends ErrorName
     case object SimpleSelectIsNotGroupableFormat extends ErrorName
     // ======================================
     //              Unknown
@@ -99,22 +99,34 @@ object Errors {
   }
 
   case class Cause(message: String)
-  case class ErrorPayload(errorCategory: ErrorCategory, error: String, message: String, causes: List[Cause], context: Option[Context]) extends ConnectorError(message) {
-    def maybeContext[C <: Context : ClassTag]: Option[C] = context collect { case c:C => c }
-    def ensureContext[C <: Context : ClassTag]: C = maybeContext[C].getOrElse(throw new ContextMismatch[C](context))
+  case class ErrorPayload(
+      errorCategory: ErrorCategory,
+      error: String,
+      message: String,
+      causes: List[Cause],
+      context: Option[Context]
+  ) extends ConnectorError(message) {
+    def maybeContext[C <: Context: ClassTag]: Option[C] = context collect { case c: C => c }
+    def ensureContext[C <: Context: ClassTag]: C        = maybeContext[C].getOrElse(throw new ContextMismatch[C](context))
   }
 
   object ErrorPayload {
-    def apply(errorCategory: ErrorCategory, error: ErrorName, message: String, causes: List[Cause], context: Option[Context]): ErrorPayload =
+    def apply(
+        errorCategory: ErrorCategory,
+        error: ErrorName,
+        message: String,
+        causes: List[Cause],
+        context: Option[Context]
+    ): ErrorPayload =
       ErrorPayload(errorCategory, error.entryName, message, causes, context)
   }
 
   sealed trait Context
   case class Fields(fields: List[String]) extends Context
 
-  case class ContextMismatch[C <: Context : ClassTag](context: Option[Context]) extends Throwable {
+  case class ContextMismatch[C <: Context: ClassTag](context: Option[Context]) extends Throwable {
     val expected = classTag[C].runtimeClass.getSimpleName
-    val actual = context.map(_.getClass().getSimpleName).getOrElse("None")
+    val actual   = context.map(_.getClass().getSimpleName).getOrElse("None")
 
     override def getMessage(): String = s"Expected `$expected`, found `$actual`"
   }

083a2bc97e53f5cbbfc1fae1976d058c548dcf6b
Laszlo Konya <laszlo.konya@emarsys.com>
2019-09-09 12:55:05 +0200

    CDP-179: remove unclear, ignored test
    Co-authored-by: Miklos Martin <miklos.martin@gmail.com>

--- bigquery/src/it/scala/com/emarsys/rbd/connector/bigquery/BigQueryRawSelectItSpec.scala
+++ bigquery/src/it/scala/com/emarsys/rbd/connector/bigquery/BigQueryRawSelectItSpec.scala
@@ -2,16 +2,14 @@ package com.emarsys.rbd.connector.bigquery
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
 import akka.testkit.TestKit
 import akka.util.Timeout
 import com.emarsys.rbd.connector.bigquery.utils.{SelectDbInitHelper, TestHelper}
 import com.emarsys.rdb.connector.test._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
+import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContextExecutor}
-import scala.util.{Failure, Try}
 
 class BigQueryRawSelectItSpec
     extends TestKit(ActorSystem("BigQueryRawSelectItSpec"))
@@ -53,25 +51,5 @@ class BigQueryRawSelectItSpec
         Seq("0", "true", "false")
       )
     }
-
-    "cancel job" ignore {
-      val slowSelect = s"""SELECT A.A1 FROM $dataset.$aTableName A
-                         |JOIN $dataset.$aTableName B ON NOT A.A1 = B.A1
-                         |JOIN $dataset.$aTableName C ON NOT A.A1 = C.A1
-                         |JOIN $dataset.$aTableName D ON NOT A.A1 = D.A1
-                         |JOIN $dataset.$aTableName E ON NOT A.A1 = E.A1
-                         |JOIN $dataset.$aTableName F ON NOT A.A1 = F.A1
-                         |JOIN $dataset.$aTableName G ON NOT A.A1 = G.A1
-                         |JOIN $dataset.$aTableName H ON NOT A.A1 = H.A1
-                         |JOIN $dataset.$aTableName I ON NOT A.A1 = I.A1
-                         |;""".stripMargin
-
-      val source = Await.result(connector.rawSelect(slowSelect, None, 13.second), 30.seconds).right.get
-
-      val result = Try(Await.result(source.runWith(Sink.seq), 70.seconds).size)
-
-      result should matchPattern { case k: Failure[_] => }
-    }
-
   }
 }

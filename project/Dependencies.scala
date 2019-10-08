43bfa983ecf5b37442664168ce696e4c1e222f92
Laszlo Konya <laszlo.konya@emarsys.com>
2019-10-08 11:30:30 +0200

    RDS-OPS: upgrade MySql driver

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val ScalaVersion = "2.12.10"
 
   val mssqlVersion    = "6.4.0.jre8"
-  val mysqlVersion    = "8.0.16"
+  val mysqlVersion    = "8.0.17"
   val postgresVersion = "42.2.8"
   val redshiftVersion = "1.2.34.1058"
 

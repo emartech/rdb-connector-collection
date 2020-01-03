bd737b4493b4b3d9489a4979d9c838e6817241e6
Miklos Martin <miklos.martin@gmail.com>
2020-01-03 19:44:18 +0100

    fix publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "$PGP_SECRET" | base64 --decode | gpg --batch --passphrase "$PGP_PASSPHRASE" --import
+echo "$PGP_SECRET" | base64 -d | gpg --batch --passphrase "$PGP_PASSPHRASE" --import
 
 sbt publishSigned

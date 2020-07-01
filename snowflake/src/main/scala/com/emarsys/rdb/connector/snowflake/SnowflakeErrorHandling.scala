b735c750a2f2376965d399931ca717873937563f
Robert Laszlo Doczi <robert.doczi@emarsys.com>
2020-07-01 14:30:52 +0200

    CDP-1068: add snowflake default error handling

---
+++ snowflake/src/main/scala/com/emarsys/rdb/connector/snowflake/SnowflakeErrorHandling.scala
@@ -0,0 +1,14 @@
+package com.emarsys.rdb.connector.snowflake
+
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+import com.emarsys.rdb.connector.common.defaults.ErrorConverter
+import com.emarsys.rdb.connector.common.models.Errors.DatabaseError
+
+trait SnowflakeErrorHandling {
+  import ErrorConverter._
+
+  protected def eitherErrorHandler[T](): PartialFunction[Throwable, Either[DatabaseError, T]] = default.andThen(Left.apply)
+
+  protected def streamErrorHandler[A]: PartialFunction[Throwable, Source[A, NotUsed]] = default.andThen(Source.failed)
+}

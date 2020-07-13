ed864bb3d06fd7efdccc960fc0d4a732578e581e
Robert Laszlo Doczi <robert.doczi@emarsys.com>
2020-07-13 10:32:35 +0200

    CDP-1173: implement SWGL using snowflake window functions
    Co-authored-by: Miklos Martin <miklos.martin@gmail.com>
    Co-authored-by: Laszlo Konya <laszlo.konya@emarsys.com>

--- snowflake/src/main/scala/com/emarsys/rdb/connector/snowflake/SnowflakeSimpleSelect.scala
+++ snowflake/src/main/scala/com/emarsys/rdb/connector/snowflake/SnowflakeSimpleSelect.scala
@@ -4,6 +4,7 @@ import akka.NotUsed
 import akka.stream.scaladsl.Source
 import com.emarsys.rdb.connector.common.ConnectorResponse
 import com.emarsys.rdb.connector.common.models.SimpleSelect
+import com.emarsys.rdb.connector.snowflake.SnowflakeSqlWriters.selectWithGroupLimitWriter
 
 import scala.concurrent.duration.FiniteDuration
 
@@ -19,5 +20,13 @@ trait SnowflakeSimpleSelect {
     streamingQuery(timeout)(select.toSql)
   }
 
-  // TODO: implement simpleSelectWithGroupLimit using window function
+  override def runSelectWithGroupLimit(
+      select: SimpleSelect,
+      groupLimit: Int,
+      references: Seq[String],
+      timeout: FiniteDuration
+  ): ConnectorResponse[Source[Seq[String], NotUsed]] = {
+    val sql = select.toSql(selectWithGroupLimitWriter(groupLimit, references))
+    streamingQuery(timeout)(sql).map(_.map(_.map(_.dropRight(1))))
+  }
 }

de8bd8cd5e3ee5755fcd8dc2c04810cc46f5ac77
Gabor Fulop <gabor.fulop@emarsys.com>
2020-07-14 12:06:59 +0200

    CDP-1184: add metadata field info test to postgresql (more detailed info here is not possible since information_schema.columns.COLUMN_TYPE does not exist)

--- postgresql/src/it/scala/com/emarsys/rdb/connector/postgresql/PostgreSqlMetadataItSpec.scala
+++ postgresql/src/it/scala/com/emarsys/rdb/connector/postgresql/PostgreSqlMetadataItSpec.scala
@@ -1,6 +1,7 @@
 package com.emarsys.rdb.connector.postgresql
 
-import com.emarsys.rdb.connector.postgresql.utils.{BaseDbSpec, TestHelper}
+import com.emarsys.rdb.connector.common.models.TableSchemaDescriptors.FieldModel
+  import com.emarsys.rdb.connector.postgresql.utils.{BaseDbSpec, TestHelper}
 import com.emarsys.rdb.connector.test.MetadataItSpec
 
 import scala.concurrent.Await
@@ -9,6 +10,7 @@ import scala.concurrent.duration._
 class PostgreSqlMetadataItSpec extends MetadataItSpec with BaseDbSpec {
 
   override val awaitTimeout = 15.seconds
+  val fieldsTable: String = "fields_table"
 
   def initDb(): Unit = {
     val createTableSql =
@@ -18,7 +20,7 @@ class PostgreSqlMetadataItSpec extends MetadataItSpec with BaseDbSpec {
          |    FirstName varchar(255),
          |    Address varchar(255),
          |    City varchar(255)
-                            |);""".stripMargin
+         |);""".stripMargin
 
     val createOtherSchemaSql = s"""CREATE SCHEMA "$otherSchema""""
 
@@ -43,12 +45,51 @@ class PostgreSqlMetadataItSpec extends MetadataItSpec with BaseDbSpec {
     val dropTableSql = s"""DROP TABLE "$tableName";"""
     val dropOtherTableSql = s"""DROP TABLE "$otherSchema"."$tableNameInOtherSchema";"""
     val dropOtherSchemaSql = s"""DROP SCHEMA "$otherSchema";"""
+    val dropFieldsTableSql = s"""DROP TABLE "$fieldsTable";"""
     Await.result(for {
       _ <- TestHelper.executeQuery(dropViewSql)
       _ <- TestHelper.executeQuery(dropTableSql)
       _ <- TestHelper.executeQuery(dropOtherTableSql)
       _ <- TestHelper.executeQuery(dropOtherSchemaSql)
+      _ <- TestHelper.executeQuery(dropFieldsTableSql)
     } yield (), 15.seconds)
   }
 
+  s"PostgreSqlMetadataItSpec $uuid" when {
+
+    "#listTablesWithFields" should {
+      "list fields with column type" in {
+        val createTableSql = s"""CREATE TABLE "$fieldsTable" (
+                                |    "PersonID" INTEGER,
+                                |    "LastName" varchar(255),
+                                |    "FirstName" text,
+                                |    "Address" float(5),
+                                |    "City" varchar(15),
+                                |    "Unsigned" BIGINT,
+                                |    "Date" DATE,
+                                |    "Timestamp" TIMESTAMP
+                                |);""".stripMargin
+
+        Await.result(TestHelper.executeQuery(createTableSql), 10.seconds)
+
+        val resultE = Await.result(connector.listTablesWithFields(), awaitTimeout)
+
+        resultE shouldBe a[Right[_, _]]
+        val result = resultE.value
+        result.map(_.name) should contain(fieldsTable)
+        result.find(_.name == fieldsTable).get.fields should be(
+          Seq(
+            FieldModel("PersonID", "integer"),
+            FieldModel("LastName", "character varying"),
+            FieldModel("FirstName", "text"),
+            FieldModel("Address", "real"),
+            FieldModel("City", "character varying"),
+            FieldModel("Unsigned", "bigint"),
+            FieldModel("Date", "date"),
+            FieldModel("Timestamp", "timestamp without time zone")
+          )
+        )
+      }
+    }
+  }
 }

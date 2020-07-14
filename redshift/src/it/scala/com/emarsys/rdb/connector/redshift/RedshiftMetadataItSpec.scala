7aac69bfc701320f28c5606b008fd210eb1e9a00
Gabor Fulop <gabor.fulop@emarsys.com>
2020-07-14 14:13:49 +0200

    CDP-1184: add metadata field info test to redshift (more detailed info here is not possible since SVV_COLUMNS has no more detailed column)

--- redshift/src/it/scala/com/emarsys/rdb/connector/redshift/RedshiftMetadataItSpec.scala
+++ redshift/src/it/scala/com/emarsys/rdb/connector/redshift/RedshiftMetadataItSpec.scala
@@ -1,5 +1,6 @@
 package com.emarsys.rdb.connector.redshift
 
+import com.emarsys.rdb.connector.common.models.TableSchemaDescriptors.FieldModel
 import com.emarsys.rdb.connector.redshift.utils.{BaseDbSpec, TestHelper}
 import com.emarsys.rdb.connector.test.MetadataItSpec
 
@@ -9,6 +10,8 @@ import scala.concurrent.Await
 class RedshiftMetadataItSpec extends MetadataItSpec with BaseDbSpec {
 
   override val awaitTimeout = 15.seconds
+
+  val fieldsTable = "fields_table"
 
   def initDb(): Unit = {
     val createTableSql =
@@ -30,25 +33,70 @@ class RedshiftMetadataItSpec extends MetadataItSpec with BaseDbSpec {
     val createViewSql = s"""CREATE VIEW "$viewName" AS
                            |SELECT PersonID, LastName, FirstName
                            |FROM "$tableName";""".stripMargin
-    Await.result(for {
-      _ <- TestHelper.executeQuery(createTableSql)
-      _ <- TestHelper.executeQuery(createOtherSchemaSql)
-      _ <- TestHelper.executeQuery(createTableOtherSchemaSql)
-      _ <- TestHelper.executeQuery(createViewSql)
-    } yield (), awaitTimeout)
+    Await.result(
+      for {
+        _ <- TestHelper.executeQuery(createTableSql)
+        _ <- TestHelper.executeQuery(createOtherSchemaSql)
+        _ <- TestHelper.executeQuery(createTableOtherSchemaSql)
+        _ <- TestHelper.executeQuery(createViewSql)
+      } yield (),
+      awaitTimeout
+    )
   }
 
   def cleanUpDb(): Unit = {
-    val dropViewSql  = s"""DROP VIEW "$viewName";"""
-    val dropTableSql = s"""DROP TABLE "$tableName";"""
-    val dropOtherTableSql = s"""DROP TABLE "$otherSchema"."$tableNameInOtherSchema";"""
+    val dropViewSql        = s"""DROP VIEW "$viewName";"""
+    val dropTableSql       = s"""DROP TABLE "$tableName";"""
+    val dropOtherTableSql  = s"""DROP TABLE "$otherSchema"."$tableNameInOtherSchema";"""
     val dropOtherSchemaSql = s"""DROP SCHEMA "$otherSchema";"""
-    Await.result(for {
-      _ <- TestHelper.executeQuery(dropViewSql)
-      _ <- TestHelper.executeQuery(dropTableSql)
-      _ <- TestHelper.executeQuery(dropOtherTableSql)
-      _ <- TestHelper.executeQuery(dropOtherSchemaSql)
-    } yield (), awaitTimeout)
+    val dropFieldsTableSql = s"""DROP TABLE "$fieldsTable";"""
+    Await.result(
+      for {
+        _ <- TestHelper.executeQuery(dropViewSql)
+        _ <- TestHelper.executeQuery(dropTableSql)
+        _ <- TestHelper.executeQuery(dropOtherTableSql)
+        _ <- TestHelper.executeQuery(dropOtherSchemaSql)
+        _ <- TestHelper.executeQuery(dropFieldsTableSql)
+      } yield (),
+      awaitTimeout
+    )
+  }
+
+  s"RedshiftMetadataItSpec $uuid" when {
+
+    "#listTablesWithFields" should {
+      "list fields with column type" in {
+        val createTableSql = s"""CREATE TABLE "$fieldsTable" (
+                                |    "PersonID" INTEGER,
+                                |    "LastName" varchar(255),
+                                |    "FirstName" text,
+                                |    "Address" float(5),
+                                |    "City" varchar(15),
+                                |    "Unsigned" BIGINT,
+                                |    "Date" DATE,
+                                |    "Timestamp" TIMESTAMP
+                                |);""".stripMargin
+
+        Await.result(TestHelper.executeQuery(createTableSql), 10.seconds)
+
+        val resultE = Await.result(connector.listTablesWithFields(), awaitTimeout)
+        resultE shouldBe a[Right[_, _]]
+        val result = resultE.value
+        result.map(_.name) should contain(fieldsTable)
+        result.find(_.name == fieldsTable).get.fields should be(
+          Seq(
+            FieldModel("personid", "integer"),
+            FieldModel("lastname", "character varying"),
+            FieldModel("firstname", "character varying"),
+            FieldModel("address", "real"),
+            FieldModel("city", "character varying"),
+            FieldModel("unsigned", "bigint"),
+            FieldModel("date", "date"),
+            FieldModel("timestamp", "timestamp without time zone")
+          )
+        )
+      }
+    }
   }
 
 }

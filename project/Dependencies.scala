c0d3ec773193c54632c115411a9cf47a95cc3b89
Scala Steward <team-rds@emarsys.com>
2021-02-25 05:06:55 +0000

    CDP-1765: Update mockito-3-4 to 3.2.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   val scalatestVersion        = "3.2.3"
   val mockitoScalaVersion     = "1.16.25"
   val mockitoVersion          = "3.7.7"
-  val scalatestMockitoVersion = "3.2.3.0"
+  val scalatestMockitoVersion = "3.2.5.0"
 
   private val defaultScalacOptions = Seq(
     "-encoding",

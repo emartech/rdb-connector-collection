bf13a99e9bfad0771117bf0bf551dbf30dd0ce60
Scala Steward <team-rds@emarsys.com>
2021-04-01 05:12:04 +0000

    CDP-1765: Update snowflake-jdbc to 3.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val mysqlVersion     = "8.0.23"
   val postgresVersion  = "42.2.19"
   val redshiftVersion  = "1.2.43.1067"
-  val snowflakeVersion = "3.13.1"
+  val snowflakeVersion = "3.13.2"
 
   val akkaVersion              = "2.6.13"
   val akkaHttpVersion          = "10.2.4"

7809a84e43daadeb6626eba8883373c7c2a3d4cc
Scala Steward <team-rds@emarsys.com>
2021-05-20 05:08:17 +0000

    CDP-1765: Update cats-core to 2.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val akkaHttpVersion          = "10.2.4"
   val akkaStreamContribVersion = "0.11"
   val slickVersion             = "3.3.3"
-  val catsCoreVersion          = "2.4.2"
+  val catsCoreVersion          = "2.6.1"
   val jwtVersion               = "5.0.0"
   val enumeratumVersion        = "1.6.1"
 

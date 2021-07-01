acaf57d992084e30c7839fb4296ea579bbcdab0b
Scala Steward <team-rds@emarsys.com>
2021-07-01 00:15:52 +0000

    CDP-1765: Update mockito-core to 3.11.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
   val scalatestVersion        = "3.2.6"
   val mockitoScalaVersion     = "1.16.32"
-  val mockitoVersion          = "3.8.0"
+  val mockitoVersion          = "3.11.2"
   val scalatestMockitoVersion = "3.2.6.0"
 
   private val defaultScalacOptions = Seq(
